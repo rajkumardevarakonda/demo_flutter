c4fc1ce88f3fae7afc961178361ec8f3bea834ac
rajkumardevarakonda <rajkumar23dev@gmail.com>
2023-05-18 22:39:46 +0530

    login page

---
+++ lib/login_page.dart
@@ -0,0 +1,99 @@
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var emailText = TextEditingController();
+  var passText = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login_Page")),
+      body: Center(
+        child: SizedBox(
+          width: 300,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextField(
+                controller: emailText,
+                decoration: InputDecoration(
+                    hintText: 'Enter email',
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(11),
+                        borderSide:  BorderSide(
+                            color:Colors.orange, width: 2)),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(11),
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 19, 19, 15), width: 2)),
+                    // disabledBorder: OutlineInputBorder(
+                    //     borderRadius: BorderRadius.circular(11),
+                    //     borderSide:  BorderSide(
+                    //         color: Color.fromARGB(255, 20, 227, 203), width: 2)),
+                    // suffixText: 'username exits',
+
+                    suffixIcon: Icon(Icons.email, color: Colors.orange,),
+                ),
+              ),
+              Container(
+                height: 11,
+              ),
+              TextField(
+                  controller: passText,
+                  obscureText: false,
+                  obscuringCharacter:'*',
+                  decoration: InputDecoration(
+                    hintText: 'Enter Password here',
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(21),
+                        borderSide: const BorderSide(
+                            color: Colors.black, width: 2),),
+                        enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(21),
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 19, 19, 15), width: 2)),
+                    suffixIcon: IconButton(
+                  icon: Icon(Icons.remove_red_eye, color: Colors.red,),
+                onPressed: () {  },
+              ),),
+              ),
+              ElevatedButton(onPressed: (){
+               String  uEmail = emailText.text.toString();
+               String uPass = passText.text;
+              }, child: Text(
+                'Login'
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyWidget extends StatelessWidget {
   const MyWidget({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,7 +14,7 @@ class MyWidget extends StatelessWidget {
         home: Scaffold(
           appBar: AppBar(
             title: const Center(
-              child: Text("Hello World"),
+              child: Text("website"),
             ),
             actions: [
               Row(
@@ -27,14 +26,14 @@ class MyWidget extends StatelessWidget {
                       Icons.search,
                       color: Colors.black54,
                     ),
-                    onPressed:() {},
+                    onPressed: () {},
                   ),
                   IconButton(
                     icon: const Icon(
                       Icons.facebook_outlined,
                       color: Colors.black54,
                     ),
-                    onPressed:() {},
+                    onPressed: () {},
                   ),
                 ],
               ),
@@ -43,93 +42,93 @@ class MyWidget extends StatelessWidget {
           ),
           body: SafeArea(
               child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Image(
+                image: AssetImage('image.jpg'),
+                height: 150,
+                width: 400.0,
+                alignment: Alignment.topLeft,
+              ),
+              const Image(
+                image: AssetImage('image1.png'),
+                height: 150,
+                width: 150.0,
+                alignment: Alignment.topLeft,
+              ),
+              Container(
+                color: const Color.fromARGB(255, 142, 18, 225),
+                width: 150.0,
+                height: 150.0,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(20.0),
+                // margin: const EdgeInsets.all(100.0),
+                child: const Text("Column: 1"),
+              ),
+              Container(
+                color: const Color.fromARGB(255, 25, 197, 155),
+                width: 150.0,
+                height: 150.0,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(20.0),
+                // margin: const EdgeInsets.all(100.0),
+                child: const Text("Column: 2"),
+              ),
+              Container(
+                color: const Color.fromARGB(255, 222, 186, 12),
+                width: 150.0,
+                height: 150.0,
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(20.0),
+                // margin: const EdgeInsets.all(100.0),
+                child: const Text("Column: 3"),
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                //
+                // const Image(image: AssetImage('images/course-08.png'),height: 150,width: 400.0,alignment: Alignment.topLeft,),
+                // const Image(image: AssetImage('images/image.png'),height: 300,width: 300.0,alignment: Alignment.topLeft,),
                 children: [
-                  const Image(
-                    image: AssetImage('image.jpg'),
-                    height: 150,
-                    width: 400.0,
-                    alignment: Alignment.topLeft,
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Container(
+                      color: const Color.fromARGB(82, 7, 8, 17),
+                      width: 150.0,
+                      height: 100.0,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(20.0),
+                      child: const Text("Column: 1"),
+                    ),
                   ),
-                  const Image(
-                    image: AssetImage('image1.png'),
-                    height: 150,
-                    width: 150.0,
-                    alignment: Alignment.topLeft,
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Container(
+                      color: const Color.fromARGB(82, 7, 8, 17),
+                      width: 150.0,
+                      height: 100.0,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(20.0),
+                      // margin: const EdgeInsets.all(100.0),
+                      child: const Text("Column: 2"),
+                    ),
                   ),
-                  Container(
-                    color: const Color.fromARGB(255, 142, 18, 225),
-                    width: 150.0,
-                    height: 150.0,
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(20.0),
-                    // margin: const EdgeInsets.all(100.0),
-                    child: const Text("Column: 1"),
-                  ),
-                  Container(
-                    color: const Color.fromARGB(255, 25, 197, 155),
-                    width: 150.0,
-                    height: 150.0,
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(20.0),
-                    // margin: const EdgeInsets.all(100.0),
-                    child: const Text("Column: 2"),
-                  ),
-                  Container(
-                    color: const Color.fromARGB(255, 222, 186, 12),
-                    width: 150.0,
-                    height: 150.0,
-                    alignment: Alignment.center,
-                    padding: const EdgeInsets.all(20.0),
-                    // margin: const EdgeInsets.all(100.0),
-                    child: const Text("Column: 3"),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    //
-                    // const Image(image: AssetImage('images/course-08.png'),height: 150,width: 400.0,alignment: Alignment.topLeft,),
-                    // const Image(image: AssetImage('images/image.png'),height: 300,width: 300.0,alignment: Alignment.topLeft,),
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Container(
-                          color: const Color.fromARGB(82, 7, 8, 17),
-                          width: 150.0,
-                          height: 100.0,
-                          alignment: Alignment.center,
-                          padding: const EdgeInsets.all(20.0),
-                          child: const Text("Column: 1"),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Container(
-                          color: const Color.fromARGB(82, 7, 8, 17),
-                          width: 150.0,
-                          height: 100.0,
-                          alignment: Alignment.center,
-                          padding: const EdgeInsets.all(20.0),
-                          // margin: const EdgeInsets.all(100.0),
-                          child: const Text("Column: 2"),
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {},
-                        child: Container(
-                          color: const Color.fromARGB(82, 7, 8, 17),
-                          width: 150.0,
-                          height: 100.0,
-                          alignment: Alignment.center,
-                          padding: const EdgeInsets.all(20.0),
-                          child: const Text("Column: 3"),
-                        ),
-                      ),
-                    ],
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Container(
+                      color: const Color.fromARGB(82, 7, 8, 17),
+                      width: 150.0,
+                      height: 100.0,
+                      alignment: Alignment.center,
+                      padding: const EdgeInsets.all(20.0),
+                      child: const Text("Column: 3"),
+                    ),
                   ),
                 ],
-              )),
+              ),
+            ],
+          )),
         ));
   }
 }
